dbc13e22c965a47d46b2d772ec7d4f7f36b49154
bvirk1357 <bikram.virk@gmail.com>
2015-08-02 21:49:14 -0700

    Working add

--- app/controllers/teachers.server.controller.js
+++ app/controllers/teachers.server.controller.js
@@ -4,12 +4,28 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-    _ = require('lodash');
+  errorHandler = require('./errors.server.controller'),
+  Teacher = mongoose.model('Teacher'),
+  _ = require('lodash');
 
 /**
  * Create a Teacher
  */
 exports.create = function(req, res) {
+
+  var teacher = new Teacher(req.body);
+
+  teacher.save(function(err){
+
+    if (err){
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.status(201).json(teacher);
+    }
+
+  });
 
 };
 
@@ -38,5 +54,15 @@ exports.delete = function(req, res) {
  * List of Teachers
  */
 exports.list = function(req, res) {
+
+  Teacher.find().exec(function(err, teachers){
+    if(err){
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(teachers);
+    }
+  });
 
 };
--- app/routes/teachers.server.routes.js
+++ app/routes/teachers.server.routes.js
@@ -1,6 +1,11 @@
 'use strict';
 
 module.exports = function(app) {
-	// Routing logic   
-	// ...
+
+  var teachers_ctrl = require('../../app/controllers/teachers.server.controller');
+
+  app.route('/teachers')
+    .get(teachers_ctrl.list)
+    .post(teachers_ctrl.create);
+
 };
